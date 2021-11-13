9d819cb95e42511d7a6559cb913b7a5da3e95393
int3nse-git <44860596+int3nse-git@users.noreply.github.com>
2021-11-12 19:11:46 -0500

    Created example config.ts

---
+++ src/config.ts
@@ -0,0 +1,20 @@
+export const token = 'botTokenHere';
+
+export const databaseURL = 'mongoDBUrlHere';
+
+export const databaseName = 'HeartBotDev';
+
+export const colors = {
+    success: 0x2dfc9f,
+    error: 0xfc2d2d
+}
+
+export const weeklyAmount = 30;
+
+export const devOnlyMode = true;
+
+export const devIDs = ['243512754541953024', '438495812922638337'];
+
+export const imageServerURL = '0.0.0.0'; // Your IP / DNS
+
+export const imageServerPort = 9998;
