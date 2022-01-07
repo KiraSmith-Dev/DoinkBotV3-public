0c485dc94f9566bc5fe11a76d4b28815c88a38f4
int3nse-git <int3nse-git@users.noreply.github.com>
2022-01-06 19:14:08 -0500

    Set global properties first so that models can overwrite global properties

--- src/models/global/global.setFunctions.ts
+++ src/models/global/global.setFunctions.ts
@@ -3,6 +3,6 @@ import * as globalStatics from '$models/global/global.statics'
 import * as globalMethods from '$models/global/global.methods'
 
 export default function setFunctions(schema: Schema, statics: any, methods: any) {
-    Object.assign(schema.statics, statics, globalStatics);
-    Object.assign(schema.methods, methods, globalMethods);
+    Object.assign(schema.statics, globalStatics, statics);
+    Object.assign(schema.methods, globalMethods, methods);
 }
